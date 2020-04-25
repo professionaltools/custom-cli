15430e649988254ea3efd65001de9148f27fd3b0
protrait <1223056605@qq.com>
2020-04-25 21:46:40 +0800

    新增直接安装依赖功能

--- bin/index.js
+++ bin/index.js
@@ -2,25 +2,25 @@
 const program = require("commander")
 const chalk = require("chalk")
 program.version(require("../package.json").version, '-v,--version')
-program.usage("<command>")
+program.name('custom').usage("[global options] command")
 
 program.command("init")
-    .description("generate a new project")
-    .alias('i')
-    .action(() => {
-      require("../command/init")()
-    })
+  .description("generate a new project")
+  .alias('i')
+  .action(() => {
+    require("../command/init")()
+  })
 program.command("use")
-    .description("generate a new project straight from a github template")
-    .alias('u')
-    .action(() => {
-      const args = program.args
-      if (program.args.length !== 2) {
-        program.help()
-      } else {
-        require("../command/use")(args[0], args[1])
-      }
-    })
+  .description("generate a new project straight from a github template")
+  .alias('u')
+  .action(() => {
+    const args = program.args
+    if (program.args.length !== 2) {
+      program.help()
+    } else {
+      require("../command/use")(args[0], args[1])
+    }
+  })
 program.on("--help", () => {
   console.log()
   console.log(chalk.bgWhite.black('  Examples:'))
--- command/common.js
+++ command/common.js
@@ -1,13 +1,22 @@
 const fs = require("fs")
+const path = require("path")
+const shell = require("shelljs")
 const chalk = require("chalk")
 const download = require('download-git-repo')
 const ora = require("ora")
 const symbols = require('log-symbols')
+const inquirer = require("inquirer")
 module.exports = (url, name, description, author) => {
   name = name.trim()
+  const spinner = ora()
   console.log(chalk.white('\n Start generating... \n'))
-  const spinner = ora("Downloading...")
-  spinner.start()
+  //  判断目录是否已存在
+  let isHasDir = fs.existsSync(path.resolve(name));
+  if (isHasDir) {
+    spinner.fail('当前目录已存在!');
+    return false;
+  }
+  spinner.start("Downloading...")
   download(url, name, err => {
     if (err) {
       spinner.fail()
@@ -31,15 +40,46 @@ module.exports = (url, name, description, author) => {
           console.error(symbols.error, chalk.red(err))
           return
         }
+        // 移除无用的文件
+        shell.rm('-rf', `${name}/.git`)
         spinner.succeed();
         console.log(symbols.success, chalk.green('project init successfully!'))
-        console.log(`
-            ${chalk.bgWhite.black('   Run This Project  ')}
-            ${chalk.yellow(`cd ${name}`)}
-            ${chalk.yellow('npm install')}
-            ${chalk.yellow('npm start')}
-          `)
-        process.exit()
+        const shellOptionList = [
+          {
+            type: "confirm",
+            message: "是否直接安装依赖",
+            name: "install",
+          }, {
+            type: "list",
+            message: "请选择依赖安装方式",
+            name: "method",
+            choices: ["npm install","cnpm install", "yarn"],
+            when: function (answers) { // 当watch为true的时候才会提问当前问题
+              return answers.install
+            }
+          }
+        ]
+        inquirer.prompt(shellOptionList).then(res => {
+          if (res.install) {
+            shell.cd(name)
+            spinner.start(`正在帮你安装依赖...`);
+            shell.exec(res.method)
+            spinner.succeed('依赖安装成功!')
+            console.log(`
+                ${chalk.bgWhite.black('   Run This Project  ')}
+                ${chalk.yellow(`cd ${name}`)}
+                ${chalk.yellow('npm start')}
+              `)
+          } else {
+            console.log(`
+                ${chalk.bgWhite.black('   Run This Project  ')}
+                ${chalk.yellow(`cd ${name}`)}
+                ${chalk.yellow('npm install or yarn or cnpm install')}
+                ${chalk.yellow('npm start')}
+              `)
+          }
+          process.exit()
+        })
       })
     })
   })
